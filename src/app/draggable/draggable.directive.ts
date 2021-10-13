53d4eff2d7477b07622043af1b795b69b06f6b39
Equipe Ti Vista <amandadyaz@gmail.com>
2021-10-13 12:18:52 -0300

    Atualizando o draggabe

--- src/app/draggable/draggable.directive.ts
+++ src/app/draggable/draggable.directive.ts
@@ -6,28 +6,36 @@ import { Directive, EventEmitter, HostBinding, HostListener, Output } from '@ang
 export class DraggableDirective {
   @HostBinding('class.draggable') draggable = true;
 
-  @Output() dragStart = new EventEmitter<PointerEvent>();
-  @Output() dragMove = new EventEmitter();
-  @Output() dragEnd = new EventEmitter();
-  
-  private dragging = false;
+  // to trigger pointer-events polyfill
+  @HostBinding('attr.touch-action') touchAction = 'none';
 
-  @HostListener('pointerdown', ['event']) onPointerDown(event: PointerEvent): void {
+  @Output() dragStart = new EventEmitter<PointerEvent>();
+  @Output() dragMove = new EventEmitter<PointerEvent>();
+  @Output() dragEnd = new EventEmitter<PointerEvent>();
+
+  @HostBinding('class.dragging') dragging = false;
+
+  @HostListener('pointerdown', ['$event'])
+  onPointerDown(event: PointerEvent): void {
     this.dragging = true;
     this.dragStart.emit(event);
   }
-  
-  @HostListener('document: pointermove', ['event']) onPointerMove(event: PointerEvent): void {
+
+  @HostListener('document:pointermove', ['$event'])
+  onPointerMove(event: PointerEvent): void {
     if (!this.dragging) {
       return;
     }
+
     this.dragMove.emit(event);
   }
 
-  @HostListener('document: pointerup', ['event']) onPointerUp(event: PointerEvent): void {
+  @HostListener('document:pointerup', ['$event'])
+  onPointerUp(event: PointerEvent): void {
     if (!this.dragging) {
       return;
     }
+
     this.dragging = false;
     this.dragEnd.emit(event);
   }
